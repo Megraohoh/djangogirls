8eb90e7937f37c3ca903697768b2ae02fd744394
Meghan Debity <mdebity@gmail.com>
2018-08-08 17:16:21 -0500

    worked on template

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post 
 
 
 
@@ -7,5 +9,6 @@ def post_list(request):
 	Author: Meghan Debity
 	Purpose: Requesting info from model to pass to template
 	"""
-
-	return render(request, 'blog/post_list.html', {})
+	# Note that posts is a variable for QuerySet
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'blog/post_list.html', {'posts': posts})
